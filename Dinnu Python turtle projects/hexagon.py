43edf5510feec0073a9f81f14601e4de8f8f10c5
DinnuBunny <dineshkandula007@gmail.com>
2022-03-18 18:43:05 +0530

    hexa 2

--- Dinnu Python turtle projects/hexagon.py
+++ Dinnu Python turtle projects/hexagon.py
@@ -1,5 +1,5 @@
 import turtle
-col = ('red','yellow','green','cyan','pink','white')
+col = ('red','yellow','green','cyan','pink','white','green','cyan','pink','white','yellow','red')
 t = turtle.Turtle()
 screen=turtle.Screen()
 screen.bgcolor('black')
@@ -7,7 +7,7 @@ t.speed(25)
 
 for i in range(150):
     t.color(col[i%6])
-    t.forward(i*1.5)
+    t.forward(i*2)
     t.left(59)
     t.width(3)
 
