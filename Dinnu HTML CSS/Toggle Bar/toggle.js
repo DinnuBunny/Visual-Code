9fb78520b5130928c5c6b335241d954f05ae0282
DinnuBunny <dineshkandula007@gmail.com>
2022-03-23 00:01:34 +0530

    toggle bar

---
+++ Dinnu HTML CSS/Toggle Bar/toggle.js
@@ -0,0 +1,9 @@
+let toogleEl = document.getElementById("toogleEl");
+let onId = document.getElementById("onIdEl");
+let offId = document.getElementById("offIdEl");
+
+toogleEl.addEventListener("click", function() {
+    onId.classList.toggle("hide");
+    offId.classList.toggle("hide");
+    toogleEl.classList.toggle("offf")
+});
